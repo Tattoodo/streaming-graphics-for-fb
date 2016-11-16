3dafdf0223d50094773af0243d2f34fbc1582cde
Julian <julian@contingent.dk>
2016-11-16 20:26:29 +0100

    moved all nodejs code

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(`/api/`, api);
 
-app.use(`/`, express.static(`dashboard`));
+app.use(`/`, express.static(`../dashboard`));
 
 app.listen(8080, function () {
     console.log('Listening on port 8080');
