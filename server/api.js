7068945d5e6ef6f2a9c05b4ff2ece340d425dab3
Julian <julian@contingent.dk>
2016-11-17 18:03:02 +0100

    coding style format

--- server/api.js
+++ server/api.js
@@ -1,15 +1,15 @@
 const path = require(`path`)
 const md5 = require('md5')
-const express = require('express');
+const express = require('express')
 const { loadNext, saveReactions, getReactions, ReactionsSerialized } = require('./fetch-data')
 
-var app = express();
+var app = express()
 
 let counts = {
   ANGRY: 0,
   WOW: 0,
   LIKE: 0,
-  LOVE: 0,
+  LOVE: 0
 }
 
 let Reaction = {
@@ -26,18 +26,20 @@ let Reaction = {
 let types = [ Reaction.LOVE, Reaction.LIKE, Reaction.ANGRY, Reaction.WOW ]
 
 function count (counts) {
-  let res = 0;
-  for (var i in counts) if (counts.hasOwnProperty(i)) {
-    res += counts[ i ]
+  let res = 0
+  for (var i in counts) {
+    if (counts.hasOwnProperty(i)) {
+      res += counts[ i ]
+    }
   }
-  return res;
+  return res
 }
 
 mockUpdate()
 function mockUpdate () {
-  let randomItem = types[ Math.floor(Math.random() * 4) ];
-  // counts[randomItem] += Math.ceil(Math.random()*3);
-  counts[ randomItem ] += 1;
+  let randomItem = types[ Math.floor(Math.random() * 4) ]
+  // counts[randomItem] += Math.ceil(Math.random()*3)
+  counts[ randomItem ] += 1
   setTimeout(mockUpdate, 300)
 }
 
@@ -45,27 +47,29 @@ app.get(`/reactions/:objectId`, (req, res) => {
   if (req.params.objectId) {
     let cacheFile = path.join(__dirname, '/download/', md5(req.params.objectId) + '.json')
 
-    getReactions(cacheFile, types).then((result) => {
+    getReactions(cacheFile, types).then(
+      (result) => {
         res.json(result)
       },
       (reason) => {
         res.status(500).json({ error: reason })
-      }).catch((ex) => {
-      res.status(500).json({ error: ex })
-    })
-  }
-  else {
+      })
+      .catch(
+        (ex) => {
+          res.status(500).json({ error: ex })
+        })
+  } else {
     res.sendStatus(400)
   }
 })
 
 app.get('/percentages', (req, res) => {
-  let totalCount = count(counts);
+  let totalCount = count(counts)
   let data = {}
 
   types.forEach((key) => {
-    let n = counts[ key ];
-    data[ key ] = Math.round(n / totalCount * 10000) / 100;
+    let n = counts[ key ]
+    data[ key ] = Math.round(n / totalCount * 10000) / 100
   })
 
   data.errorMargin = 0
@@ -76,16 +80,15 @@ app.get('/percentages', (req, res) => {
 
   res.json({
     data: data
-  });
+  })
 })
 
 app.get('/start/:objectId', (req, res) => {
-  let url = `/${req.params.objectId}/reactions?fields=type&summary=total_count&limit=5000`;
+  let url = `/${req.params.objectId}/reactions?fields=type&summary=total_count&limit=5000`
 
   let cacheFile = path.join(__dirname, '/download/', md5(req.params.objectId) + '.json')
 
   loadNext(url).then((result) => {
-
     console.log('--- results: ' + result.data.length)
     if (result.data.length < 10) {
       console.log(result)
@@ -96,7 +99,6 @@ app.get('/start/:objectId', (req, res) => {
     dbFormat.data = result.data
     dbFormat.cursors = result.cursors
     return saveReactions(dbFormat, cacheFile)
-
   }, (reason) => {
     console.log(reason)
     res.sendStatus(500)
@@ -107,7 +109,6 @@ app.get('/start/:objectId', (req, res) => {
       console.log(reason)
       res.sendStatus(500)
     })
-
 })
 
 app.get('/stop', (req, res) => {
