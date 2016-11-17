d9136eb5b6fca6be490a11ee1e10fe3d812b33ff
Julian <julian@contingent.dk>
2016-11-17 18:20:00 +0100

    format code style

--- server/init-facebook.js
+++ server/init-facebook.js
@@ -1,13 +1,14 @@
 const config = require('./our-config.json')
-const {Facebook, FacebookApiException} = require('fb')
+const { Facebook } = require('fb')
 
-function initFacebook() {
-    let fb = new Facebook({version: config.version});
+function initFacebook () {
+  let fb = new Facebook({ version: config.version })
 
-    if (config.access_token)
-        fb.setAccessToken(config.access_token);
+  if (config.access_token) {
+    fb.setAccessToken(config.access_token)
+  }
 
-    return fb
+  return fb
 }
 
 module.exports = initFacebook
