4ae59f28b0ee1b05d13b3a0392cf68635812dabe
Julian <julian@contingent.dk>
2016-11-17 17:58:42 +0100

    whitespace reformat

--- server/api.js
+++ server/api.js
@@ -1,15 +1,15 @@
 const path = require(`path`)
 const md5 = require('md5')
 const express = require('express');
-const {loadNext, saveReactions, getReactions, ReactionsSerialized} = require('./fetch-data')
+const { loadNext, saveReactions, getReactions, ReactionsSerialized } = require('./fetch-data')
 
 var app = express();
 
 let counts = {
-    ANGRY: 0,
-    WOW: 0,
-    LIKE: 0,
-    LOVE: 0,
+  ANGRY: 0,
+  WOW: 0,
+  LIKE: 0,
+  LOVE: 0,
 }
 
 let Reaction = {
@@ -23,22 +23,22 @@ let Reaction = {
   SAD: `SAD`
 }
 
-let types = [Reaction.LOVE, Reaction.LIKE, Reaction.ANGRY, Reaction.WOW]
+let types = [ Reaction.LOVE, Reaction.LIKE, Reaction.ANGRY, Reaction.WOW ]
 
-function count(counts) {
-    let res = 0;
-    for (var i in counts) if (counts.hasOwnProperty(i)) {
-        res += counts[i]
-    }
-    return res;
+function count (counts) {
+  let res = 0;
+  for (var i in counts) if (counts.hasOwnProperty(i)) {
+    res += counts[ i ]
+  }
+  return res;
 }
 
 mockUpdate()
-function mockUpdate() {
-    let randomItem = types[Math.floor(Math.random()*4)];
-    // counts[randomItem] += Math.ceil(Math.random()*3);
-    counts[randomItem] += 1;
-    setTimeout(mockUpdate, 300)
+function mockUpdate () {
+  let randomItem = types[ Math.floor(Math.random() * 4) ];
+  // counts[randomItem] += Math.ceil(Math.random()*3);
+  counts[ randomItem ] += 1;
+  setTimeout(mockUpdate, 300)
 }
 
 app.get(`/reactions/:objectId`, (req, res) => {
@@ -60,24 +60,23 @@ app.get(`/reactions/:objectId`, (req, res) => {
 })
 
 app.get('/percentages', (req, res) => {
-    let totalCount = count(counts);
-    let data = {}
+  let totalCount = count(counts);
+  let data = {}
 
-    types.forEach((key)=>{
-        let n = counts[key];
-        data[key] = Math.round( n / totalCount * 10000) / 100;
-    })
+  types.forEach((key) => {
+    let n = counts[ key ];
+    data[ key ] = Math.round(n / totalCount * 10000) / 100;
+  })
 
-    data.errorMargin = 0
-    types.forEach((key)=>{
-        data.errorMargin += data[key]
-    })
-    data.errorMargin = Math.abs( data.errorMargin - 100 )
+  data.errorMargin = 0
+  types.forEach((key) => {
+    data.errorMargin += data[ key ]
+  })
+  data.errorMargin = Math.abs(data.errorMargin - 100)
 
-
-    res.json({
-        data: data
-    });
+  res.json({
+    data: data
+  });
 })
 
 app.get('/start/:objectId', (req, res) => {
@@ -85,28 +84,28 @@ app.get('/start/:objectId', (req, res) => {
 
   let cacheFile = path.join(__dirname, '/download/', md5(req.params.objectId) + '.json')
 
-    loadNext(url).then((result) => {
+  loadNext(url).then((result) => {
 
-        console.log('--- results: ' + result.data.length)
-        if (result.data.length < 10) {
-            console.log(result)
-            console.log('---')
-        }
+    console.log('--- results: ' + result.data.length)
+    if (result.data.length < 10) {
+      console.log(result)
+      console.log('---')
+    }
 
-        let dbFormat = new ReactionsSerialized()
-        dbFormat.data = result.data
-        dbFormat.cursors = result.cursors
-        return saveReactions(dbFormat, cacheFile)
+    let dbFormat = new ReactionsSerialized()
+    dbFormat.data = result.data
+    dbFormat.cursors = result.cursors
+    return saveReactions(dbFormat, cacheFile)
 
-    }, (reason) => {
-        console.log(reason)
-        res.sendStatus(500)
-    })
+  }, (reason) => {
+    console.log(reason)
+    res.sendStatus(500)
+  })
     .then(function () {
-        res.sendStatus(200)
-    },(reason) => {
-        console.log(reason)
-        res.sendStatus(500)
+      res.sendStatus(200)
+    }, (reason) => {
+      console.log(reason)
+      res.sendStatus(500)
     })
 
 })
@@ -114,7 +113,5 @@ app.get('/start/:objectId', (req, res) => {
 app.get('/stop', (req, res) => {
 
 })
-
-
 
 module.exports = app
