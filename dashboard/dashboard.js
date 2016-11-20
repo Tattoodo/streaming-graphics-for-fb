f06b5740bdb976c0351b131dab7f2d647904e3f3
Julian <julian@contingent.dk>
2016-11-20 16:22:38 +0100

    whitespace

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,14 +1,14 @@
 angular
-    .module('app', ['ngMaterial'])
-    .config(function($mdThemingProvider){
-        $mdThemingProvider.theme('default')
-            .primaryPalette('blue')
-            .accentPalette('red');
-    });
+  .module('app', [ 'ngMaterial' ])
+  .config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue')
+      .accentPalette('red');
+  });
 
 // settings
 angular.module('app').component('reactionsObjectId', {
-        template: `<div layout="row">
+    template: `<div layout="row">
     <div flex="initial">
      <md-input-container>
         <label>Permalink or object ID</label>
@@ -21,84 +21,82 @@ angular.module('app').component('reactionsObjectId', {
             <md-button ng-click="$ctrl.stop(); $ctrl.running=false" ng-show="$ctrl.running" ng-disabled="$ctrl.loading" ng-bind="($ctrl.loading) ? 'loading...' : 'stop'"></md-button>
     </div>
 </div>`,
-        controller: function ($http, $timeout, Storage, SceneState) {
-            let loading = 0
+    controller: function ($http, $timeout, Storage, SceneState) {
+      let loading = 0
 
-            this.running = false
+      this.running = false
 
-            let delay = 10000 // scraping interval
+      let delay = 10000 // scraping interval
 
-            this.isOpen = function () {
-              return SceneState.sceneRef !== null
-            }
+      this.isOpen = function () {
+        return SceneState.sceneRef !== null
+      }
 
-            this.start = function () {
-              Storage.started = new Date().toISOString()
+      this.start = function () {
+        Storage.started = new Date().toISOString()
 
-              SceneState.postMessage(`start`);
-            }
+        SceneState.postMessage(`start`);
+      }
 
-            this.run = function (objectId) {
-                if ((objectId+"").indexOf(`http`) > -1) {
-                  objectId = this.extractObjectId(objectId)
-                }
-
-                let since = (Storage.started) ? Storage.started : 0;
-
-                loading++
-                $http.get(`/api/start/${objectId}/${since}?access_token=${Storage.accessToken}`).then(()=>{
-                  if (this.running) {
-                    $timeout( ()=>{
-                      this.run(objectId)
-                    }, delay)
-                  }
-                }, ()=> {
-                  this.running = false
-                }).finally(() => {
-                    loading--
-                })
-            }
-
-            this.stop = () => {
-              loading++
-              $http.get(`/api/stop`).finally(() => {
-                loading--
-              });
-
-              SceneState.postMessage(`stop`, {
-                percentages: {LIKE: 25, LOVE: 25.1, WOW: 25.52}
-              });
-            }
-
-            this.extractObjectId = function(permalink) {
-              let objectId = null;
-
-              let matches = permalink.match(/\/(\d+)/g)
-              if (matches instanceof Array) {
-                  objectId = matches.join(`_`).replace(/\//g,``)
-              }
-              console.log(`extracted ${objectId}`)
-
-              return objectId
-            }
-
-            Object.defineProperty(this, `loading`, {
-              get: () => loading > 0
-            })
-
-            Object.defineProperty(this, `objectId`, {
-              get: () => localStorage.objectId,
-              set: (value) => localStorage.objectId = value
-            })
+      this.run = function (objectId) {
+        if ((objectId + "").indexOf(`http`) > -1) {
+          objectId = this.extractObjectId(objectId)
         }
+
+        let since = (Storage.started) ? Storage.started : 0;
+
+        loading++
+        $http.get(`/api/start/${objectId}/${since}?access_token=${Storage.accessToken}`).then(() => {
+          if (this.running) {
+            $timeout(() => {
+              this.run(objectId)
+            }, delay)
+          }
+        }, () => {
+          this.running = false
+        }).finally(() => {
+          loading--
+        })
+      }
+
+      this.stop = () => {
+        loading++
+        $http.get(`/api/stop`).finally(() => {
+          loading--
+        });
+
+        SceneState.postMessage(`stop`, {
+          percentages: { LIKE: 25, LOVE: 25.1, WOW: 25.52 }
+        });
+      }
+
+      this.extractObjectId = function (permalink) {
+        let objectId = null;
+
+        let matches = permalink.match(/\/(\d+)/g)
+        if (matches instanceof Array) {
+          objectId = matches.join(`_`).replace(/\//g, ``)
+        }
+        console.log(`extracted ${objectId}`)
+
+        return objectId
+      }
+
+      Object.defineProperty(this, `loading`, {
+        get: () => loading > 0
+      })
+
+      Object.defineProperty(this, `objectId`, {
+        get: () => localStorage.objectId,
+        set: (value) => localStorage.objectId = value
+      })
     }
+  }
 );
-
-
 
 // open scene button
 angular.module('app').component('openScene', {
-        template: `
+    template: `
                     <button class="md-button md-raised"
                      ng-class="{'md-primary': $ctrl.loggedin}"
                      ng-disabled="$ctrl.loggedin"
@@ -107,47 +105,49 @@ angular.module('app').component('openScene', {
                     <button ng-click="$ctrl.open()"                           
                            class="md-button md-raised md-primary">open scene</button>
                     <span ng-bind="$ctrl.isConnected() ? 'Connected' : 'Disconnected'"></span>`,
-        controller: function (SceneState, Storage, $window, $scope) {
-            this.open = function () {
-                SceneState.sceneRef = $window.open(SceneState.target, '_blank',
-                    'width=1024,height=576,location=no,status=no,menubar=no'); // note the window dimensions of the scene is modified by the scene itself
-            };
+    controller: function (SceneState, Storage, $window, $scope) {
+      this.open = function () {
+        SceneState.sceneRef = $window.open(SceneState.target, '_blank',
+          'width=1024,height=576,location=no,status=no,menubar=no'); // note the window dimensions of the scene is modified by the scene itself
+      };
 
-            this.fblogin = () => {
-              FB.getLoginStatus((response) => {
-                if (response.status === 'connected') {
-                  let accessToken = response.authResponse.accessToken
-                  Storage.accessToken = accessToken
-                  $scope.$apply(() => {
-                    this.loggedin = true
-                  })
-                }
-                else {
-                  FB.login()
-                }
-              })
-            }
+      this.fblogin = () => {
+        FB.getLoginStatus((response) => {
+          if (response.status === 'connected') {
+            let accessToken = response.authResponse.accessToken
+            Storage.accessToken = accessToken
+            $scope.$apply(() => {
+              this.loggedin = true
+            })
+          }
+          else {
+            FB.login()
+          }
+        })
+      }
 
-            this.isConnected = function () {
-                return !!SceneState.sceneRef;
-            }
-        }
+      this.isConnected = function () {
+        return !!SceneState.sceneRef;
+      }
     }
+  }
 );
-
 
 // Storage
 angular.module('app').service('Storage', class Storage {
   static get accessToken () {
     return localStorage.access_token
   }
+
   static set accessToken (value) {
     localStorage.access_token = value
   }
+
   static get started () {
     return localStorage.started
   }
-  static set started(value) {
+
+  static set started (value) {
     localStorage.started = value
   }
 });
@@ -157,14 +157,15 @@ class SceneState {
     return location + 'scene1.html'
   }
 
-  get sceneRef() {
+  get sceneRef () {
     return this._sceneRef || null
   }
-  set sceneRef(value) {
+
+  set sceneRef (value) {
     this._sceneRef = value
   }
 
-  postMessage(type, data) {
+  postMessage (type, data) {
     this._sceneRef.postMessage(new SceneState.Message(type, data), this.target);
   }
 }
