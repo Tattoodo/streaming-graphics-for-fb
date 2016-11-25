7893b2205de8cff6fe69e9edd62845e3ac23676d
Julian <julian@contingent.dk>
2016-11-25 13:38:29 +0100

    remove bower, use npm

--- server/index.js
+++ server/index.js
@@ -8,6 +8,20 @@ app.use(`/api/`, api)
 
 app.use(`/`, express.static(path.join(__dirname, `/../dashboard`)))
 
+function requireAdmin (req, res, next) {
+  console.log(req.path)
+  if (req.path.indexOf(`.js`) > -1 ||
+    req.path.indexOf(`.css`) > -1) {
+    next()
+  }
+  else {
+    next(new Error("Permission denied."))
+  }
+}
+
+app.use(`/node_modules/`, requireAdmin)
+app.use(`/node_modules/`, express.static(path.join(__dirname, `/../node_modules`)))
+
 app.listen(8080, function () {
   console.log('Listening on port 8080')
 })
