48b05cc7ca1d68a896a96960739308136c60e860
Julian <julian@contingent.dk>
2016-11-28 15:07:53 +0100

    refactor; convert to arrow functions

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -101,29 +101,22 @@ angular.module('app').component('reactionsObjectId', {
 angular.module('app').component('selectReactions', {
   template: `
 <h4>Reactions</h4>
-  <div layout="row" layout-wrap>
-    <div flex="100" layout="column">
-        <div layout="row" layout-wrap flex>
-            <div class="demo-select-all-checkboxes" flex="100" ng-repeat="item in items">
-              <md-checkbox ng-checked="exists(item, selected)" ng-click="toggle(item, selected)">
-               {{ item }}
-              </md-checkbox>
-            </div>
-        </div>
-      </div>  
-  </div>
-  <md-button ng-click="apply()">apply</md-button>
+<div ng-repeat="item in $ctrl.items">
+  <md-checkbox ng-checked="$ctrl.exists(item, $ctrl.selected)" ng-click="$ctrl.toggle(item, $ctrl.selected)">
+   {{ ::item }}
+  </md-checkbox>
+</div>
+<md-button ng-click="$ctrl.apply()">apply</md-button>
 
 `,
-  controller: function ($scope, $http, Storage) {
+  controller: function ($http, Storage) {
     let allReactions = [ Reaction.LIKE, Reaction.LOVE, Reaction.HAHA, Reaction.WOW, Reaction.SAD, Reaction.ANGRY ]
-    console.log(allReactions)
 
-    $scope.items = allReactions
+    this.items = allReactions
     let saved = Storage.reactions
-    $scope.selected = saved || allReactions.slice(0, 2)
+    this.selected = saved || allReactions.slice(0, 2)
 
-    $scope.toggle = function (item, list) {
+    this.toggle = (item, list) => {
       var idx = list.indexOf(item)
       if (idx > -1) {
         list.splice(idx, 1)
@@ -132,22 +125,18 @@ angular.module('app').component('selectReactions', {
       }
     }
 
-    $scope.apply = function () {
-      $http.put(`/api/reaction_types/${$scope.selected.join(`,`)}`, ``).then(function (response) {
-        $scope.selected = response.data
+    this.apply = () => {
+      $http.put(`/api/reaction_types/${this.selected.join(`,`)}`, ``).then((response) => {
+        this.selected = response.data
         Storage.reactions = response.data // validated
       }, console.error)
     }
 
-    $scope.exists = function (item, list) {
+    this.exists = (item, list) => {
       return list.indexOf(item) > -1
     }
 
-    $scope.isChecked = function () {
-      return $scope.selected.length === $scope.items.length
-    }
-
-    $scope.apply()
+    this.apply()
   }
 })
 
@@ -188,7 +177,7 @@ angular.module('app').component('openScene', {
 })
 
 // Storage
-angular.module('app').service('Storage', class Storage {
+angular.module('app').service('Storage', class {
   get accessToken () {
     return localStorage.access_token
   }
